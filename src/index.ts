d193942e56a0b680346cba5fece602e0c9a93ffb
Vikrant Pareek <vikrantpareek2005@gmail.com>
2026-05-03 16:15:22 +0530

    Learnt how to define types

--- src/index.ts
+++ src/index.ts
@@ -16,3 +16,10 @@ function abc(user: { name: string; age: number }): void {
 function random(): void {
   console.log("random");
 }
+
+// To define type of a function which returns a function which returns nothing
+function random2(): () => void {
+  return () => {
+    console.log("Random2");
+  };
+}
