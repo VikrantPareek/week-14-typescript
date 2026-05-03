8fb3a867abf7de16f54ce2a7175469c91ab7cd37
Vikrant Pareek <vikrantpareek2005@gmail.com>
2026-05-03 16:12:50 +0530

    Learnt how to define types

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,18 @@
-function greet(): void {
-    console.log("It is just the begining");
+// To define type of an object
+let user: {
+  name: string;
+  age: number;
+} = {
+  name: "Vinay",
+  age: 20,
+};
+
+// To define type of object in an function
+function abc(user: { name: string; age: number }): void {
+  console.log("Hello " + user.name);
 }
 
-function afterOne(fn: () => void){
-    setTimeout(fn, 5000)
+// To define type of a function which returns nothing
+function random(): void {
+  console.log("random");
 }
-
-afterOne(greet)
